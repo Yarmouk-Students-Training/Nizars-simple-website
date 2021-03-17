90bf85bdcf14d296e3b10aad04a7e1865ce06c73
nizarmashtooli <nizarmashtooli@gmail.com>
2021-03-17 19:39:14 +0200

    use mongodb to add and show blogs

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog=require('./modules/blogs');
+
 const app = express();
+const dpurl = "mongodb+srv://nizar1:test1234@nizarcluster.nc8uf.mongodb.net/node?retryWrites=true&w=majority";
 
+mongoose.connect(dpurl ,{useNewUrlParser:true,useUnifiedTopology:true})
+  .then((res) => app.listen(3000))
+  .catch((err)=>console.log("Connection error" , err));
 
-app.listen(3000);
+var db = mongoose.connection;
+ 
+
 
 app.use(express.static('Public_Files'));
 app.set('view engine', 'ejs');
 
+app.get('/add-blog', (req, res) => {
+const blog = new Blog({
+  title : 'new blog',
+  snippet:"about my new blog",
+  body : "more about my new blog"
+});
+
+blog.save()
+.then((result)=>{
+  res.send(result)
+})
+.catch((err)=>console.log(err));
+});
+
+app.get('/all-blogs', (req, res) => {  
+  Blog.find()
+  .then((result)=>res.send(result))
+  .catch((err)=>console.log(err));
+  });
+
+  app.get('/single-blog', (req, res) => {  
+    Blog.findById('60523b3caca5fe16c41e9910')
+    .then((result)=>res.send(result))
+    .catch((err)=>console.log(err));
+    });  
 
 app.get('/', (req, res) => {
   const blogs = [
