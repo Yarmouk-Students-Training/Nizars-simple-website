c52d2d7fcace87d9a7cd67cd0ca511092d41df5a
nizarmashtooli <nizarmashtooli@gmail.com>
2021-03-24 20:43:13 +0200

    MVC

---
+++ models/user.js
@@ -0,0 +1,77 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({Post:Post,Comment:Comment,Reaction:Reaction , Relationship : Relationship}) {
+      this.hasMany(Post,{foreignKey:'user_id'});
+      this.hasMany(Comment,{foreignKey:'user_id'});
+      this.hasMany(Reaction,{foreignKey:'user_id'});
+
+      this.belongsToMany(this,{through : Relationship ,as: 'actionMaker' ,foreignKey:"actionReceiver"})
+      
+    }
+  };
+  User.init({
+    user_id: {
+      allowNull: false,
+      
+      primaryKey: true,
+      type: DataTypes.STRING
+    },
+
+    first_name: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    
+    last_name: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    
+    email: {
+      unique:true,
+      allowNull: false,
+      type: DataTypes.STRING,
+
+      validate:{
+        isEmail:true
+      }
+    },
+    
+    password_hash: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    
+    gender: {
+      allowNull: false,
+      type: DataTypes.ENUM('Male' , 'Female')
+    },
+
+    country: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    DOB:{
+      allowNull:false,
+      type:DataTypes.DATE
+    }
+  
+  }
+    ,
+     
+    {
+    sequelize,
+    tableName:'user',
+    modelName: 'User',
+  });
+  return User;
+};
